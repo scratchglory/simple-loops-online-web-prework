4ed0507d31dc9c5f4311e03ca6ca4eab978e64bb
Adella <adellakenn@gmail.com>
2019-04-25 15:19:51 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -16,6 +16,7 @@ def times_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   7.times do
     puts "Welcome to Flatiron School's Web Development Course!"
+  end
 end
 
 def while_iterator(number_of_times)
